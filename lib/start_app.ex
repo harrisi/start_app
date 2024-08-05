e6eccfb33dfaa61da899a1114d219a6ba73105fe
Ian Harris <ian@fofgof.xyz>
2024-08-05 16:01:01 -0700

    remove default module contents

--- lib/start_app.ex
+++ lib/start_app.ex
@@ -1,18 +1,2 @@
 defmodule StartApp do
-  @moduledoc """
-  Documentation for `StartApp`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> StartApp.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
